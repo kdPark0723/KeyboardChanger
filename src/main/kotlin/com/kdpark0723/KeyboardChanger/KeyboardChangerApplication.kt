6d1cb14e6ca7f3618e14f1cf93090e3e1fd12470
박시율 <kdpark0723@gmail.com>
2019-10-17 15:59:33 +0900

    init spring webflux

---
+++ src/main/kotlin/com/kdpark0723/KeyboardChanger/KeyboardChangerApplication.kt
@@ -0,0 +1,11 @@
+package com.kdpark0723.KeyboardChanger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeyboardChangerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KeyboardChangerApplication>(*args)
+}
